344b61923ba5186b2ccd4a308d516d0090bb1f26
Simon Polack <simon.polack@profitbricks.com>
2017-09-19 14:57:56 +0200

    general: Ship FDroid and Privileged Extension

--- targets/general/patches/microg-add-to-product-packages.sh
+++ targets/general/patches/microg-add-to-product-packages.sh
@@ -1,1 +1,5 @@
+# Add microG Packages
 echo -e 'PRODUCT_PACKAGES += \\\n    GmsCore \\\n    GsfProxy \\\n    FakeStore' >> build/target/product/base.mk
+
+# Add F-Droid
+echo -e 'PRODUCT_PACKAGES += \\\n    FDroidPrivilegedExtension \\\n    FDroid' >> build/target/product/base.mk
