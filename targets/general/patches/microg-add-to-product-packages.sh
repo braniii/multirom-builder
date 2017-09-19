afcd3503b264a6b6010017d47538181a5f7916c9
Simon Polack <simon.polack@profitbricks.com>
2017-09-19 15:28:59 +0200

    general: Ship DroidGuardHelper

--- targets/general/patches/microg-add-to-product-packages.sh
+++ targets/general/patches/microg-add-to-product-packages.sh
@@ -3,3 +3,6 @@ echo -e 'PRODUCT_PACKAGES += \\\n    GmsCore \\\n    GsfProxy \\\n    FakeStore'
 
 # Add F-Droid
 echo -e 'PRODUCT_PACKAGES += \\\n    FDroidPrivilegedExtension \\\n    FDroid' >> build/target/product/base.mk
+
+# Add DroidGuard Helper
+echo -e 'PRODUCT_PACKAGES += \\\n    DroidGuardHelper' >> build/target/product/base.mk
