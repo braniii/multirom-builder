600832cfa7401a23562327847d5b1ff2bace0764
Simon Polack <simon.polack@profitbricks.com>
2017-09-22 14:41:26 +0200

    disable FDroidPrivilegedExtension
    It caused Issues.
    https://forum.xda-developers.com/showpost.php?p=73887989&postcount=39

--- targets/general/patches/microg-add-to-product-packages.sh
+++ targets/general/patches/microg-add-to-product-packages.sh
@@ -2,7 +2,8 @@
 echo -e 'PRODUCT_PACKAGES += \\\n    GmsCore \\\n    GsfProxy \\\n    FakeStore' >> build/target/product/base.mk
 
 # Add F-Droid
-echo -e 'PRODUCT_PACKAGES += \\\n    FDroidPrivilegedExtension \\\n    FDroid' >> build/target/product/base.mk
+echo -e 'PRODUCT_PACKAGES += \\\n    FDroid' >> build/target/product/base.mk
+#echo -e 'PRODUCT_PACKAGES += \\\n    FDroidPrivilegedExtension \\\n    FDroid' >> build/target/product/base.mk
 
 # Add DroidGuard Helper
 echo -e 'PRODUCT_PACKAGES += \\\n    DroidGuardHelper' >> build/target/product/base.mk
